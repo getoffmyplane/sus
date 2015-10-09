5fb30e08ff7e484331184ee1a8a76b0a1511f107
getoffmyplane <luke.woods@outlook.com>
2015-10-09 14:44:52 +0100

    0.1.1.4: update fonts to Roboto & Roboto Slab - Added admin-style.css to twentyfifteen-child - Added function to admin_enqueue_scripts to child functions.php - From now on, modify all CSS in the child theme

--- wp-content/themes/twentyfifteen-child/functions.php
+++ wp-content/themes/twentyfifteen-child/functions.php
@@ -1,9 +1,14 @@
 <?php
 
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+add_action('wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
-wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
 
+add_action('admin_enqueue_scripts', 'admin_css_overrides');
+
+function admin_css_overrides() {
+    wp_enqueue_style( 'admin-style',  get_stylesheet_directory_uri() . '/admin-style.css' );
 }
 
 ?>
